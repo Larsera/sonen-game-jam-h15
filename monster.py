afe9289130722604c7ca1fbb3e8fcf888bcb8409
aasmunjr <aasmunjr@ulrik.uio.no>
2015-10-23 23:55:19 +0200

    is alive dead

--- monster.py
+++ monster.py
@@ -7,5 +7,18 @@ class Monster(self):
         self.action_points = acp
         self.venomous = ven
         self.name = name
+        self.alive = True
 
 
+    def take_damage(self, dmg):
+        self.hp -= dmg
+
+    def is_alive(self):
+        return self.alive
+
+    def dead(self):
+        if hp <= 0:
+            self.alive = False
+            self.action_points = 0
+
+        return not self.alive
