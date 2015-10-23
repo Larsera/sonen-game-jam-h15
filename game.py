5c9874afa478fe528bb163dff1be8d0d4300734f
larsera <larsera@gmail.com>
2015-10-23 22:39:08 +0200

    fixed some bugs to prepare for runnig the game

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame, math, sys, os
 from pygame.locals import *
 
 from world import *
+from character import *
 
 screen = pygame.display.set_mode((1366, 768))
 clock = pygame.time.Clock()
@@ -24,7 +25,8 @@ class Block(pygame.sprite.Sprite):
 
 
 events = EventController()
-world = World(32, 32, os.path.join('img', 'tileset.jpg'), screen)
+worldpath = os.path.join('img', 'tileset.jpg')
+world = World(32, 32, worldpath, screen)
 character = Character(os.path.join('img', 'character.png'), (10, 10), screen)
 
 running = 1
