646c27df7106bb3ca0bc92e3d768ad9c4281d152
larsera <larsera@gmail.com>
2015-10-23 20:03:00 +0200

    added draw method to character

--- character.py
+++ character.py
@@ -1,9 +1,22 @@
 import pygame
+from pygame.locals import *
+from pygame import Rect
 
 class Character():
-    def __init__(self, image, position):
+
+    def __init__(self, image, position, screen):
+        self.screen = screen
         self.src_image = pygame.image.load(image)
+        self.image = src_image.convert()
+
         self.position = position
-        self.speed = self.direction = 0
+    
+    def draw(self):
+        self.screen.blit(self.image, position)
+
+    def move(direction):
+        self.position = (0,0)
+
+    # def update():
 
 
