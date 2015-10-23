b7537845435a58847a7c5f244837217deae0dbfb
larsera <larsera@gmail.com>
2015-10-23 23:36:12 +0200

    We has graphics

--- game.py
+++ game.py
@@ -4,7 +4,9 @@ from pygame.locals import *
 from world import *
 from character import *
 
-screen = pygame.display.set_mode((1366, 768))
+pygame.init()
+_screen = pygame.display.set_mode((1366, 768))
+screen = pygame.Surface(_screen.get_size())
 clock = pygame.time.Clock()
 
 class EventController():
@@ -23,16 +25,23 @@ class Block(pygame.sprite.Sprite):
         self.src_image = pygame.image.load(image)
         self.image = self.src_image.convert()
 
-
 events = EventController()
 world = World(32, 32, os.path.join('img', 'tileset.jpg'), screen)
 character = Character(os.path.join('img', 'character.png'), (10, 10), screen)
+image = os.path.join('img', 'character.png')
+src_image = pygame.image.load(image)
+image = src_image.convert()
 
 running = 1
 while running:
+    
+    screen.blit(image, (10, 10))
     events.handleEvents()
     character.update()
     world.draw()
     character.draw()
+
+    _screen.blit(screen, (0,0))
+    pygame.display.flip()
 
 pygame.quit()
