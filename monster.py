c3f1dbdc05304bb194dd87a48c439d71f81b0399
aasmunjr <aasmunjr@ulrik.uio.no>
2015-10-23 22:40:35 +0200

    monsters

---
+++ monster.py
@@ -0,0 +1,11 @@
+
+class Monster(self):
+
+    def __init__(self, (hp, dmg, acp, ven), name):
+        self.hp = hp
+        self.damage = dmg
+        self.action_points = acp
+        self.venomous = ven
+        self.name = name
+
+
--- tiles.py
+++ tiles.py
@@ -11,12 +11,10 @@ class Tile(self):
 
 # TODO: real values
 def get_tile(ident):
-    return {
-            0 : Tile((0, 0, 0, 0, 0, 0), "empty desert"),
+    return {0 : Tile((0, 0, 0, 0, 0, 0), "empty desert"),
             1 : Tile((0, 0, 0, 0, 0, 0), "big rock"),
             2 : Tile((0, 0, 0, 0, 0, 0), "small rock"),
             3 : Tile((0, 0, 0, 0, 0, 0), "jagged rock"),
             4 : Tile((0, 0, 0, 0, 0, 0), "oasis"),
             5 : Tile((0, 0, 0, 0, 0, 0), "abandoned camp"),
-            6 : Tile((0, 0, 0, 0, 0, 0), "pyramid")
-            }[ident]
+            6 : Tile((0, 0, 0, 0, 0, 0), "pyramid")}[ident]
