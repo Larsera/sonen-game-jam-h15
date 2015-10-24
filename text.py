0d080af7b6636cc50a884fadcd46de8ff25d5a55
vebjorrs <vebjorrs@ifi.uio.no>
2015-10-25 00:43:51 +0200

    Added intro text.

---
+++ text.py
@@ -0,0 +1,6 @@
+intro = "\"Get on and I'll show you all the sights of the desert!\" the old " \
+        "man proclaimed as you climbed onto his tired-looking camel. That " \
+        "was five hours ago. Now, you can only watch as the old man rides " \
+        "away, leaving you stranded in the middle of the barren wasteland. " \
+        "\"That's what happens when you can't pay!\" you hear him shout as " \
+        "he disappears behind a sand dune."
