b24c0dc6ec0a878dd167c252abba915e1dda988f
vebjorrs <vebjorrs@ifi.uio.no>
2015-10-25 01:42:55 +0200

    Removed weapon descriptions as these can now be found in text.py.

--- items.py
+++ items.py
@@ -2,21 +2,16 @@
 class weapon():
 
 
-    def __init__(self, name, dmg=1, desc=""):
+    def __init__(self, name, dmg=1):
         self.name = name
         self.dmg = dmg
-        self.desc = desc
 
     def get_weapon(ident):
-        return {"stick" : weapon("Stick", desc="A thin stick. Not very effective."),
-            "sharp_rock" : weapon("Sharp rock", dmg=2, 
-            desc="Better than a stick, but you have to get too close for comfort."),
-            "pointy_stick" : weapon("Pointy stick", dmg=3,
-            desc="A sharpened stick. Perfect for poking things."),
-            "plusone_mace" : weapon("+1 Mace", dmg=4,
-            desc="A bottle of mace."),
-            "warped_blade" : weapon("Warped blade", dmg=5,
-            desc="Not to be confused with the Vorpal blade.")}.get(ident)
+        return {"stick" : weapon("Stick"),
+            "sharp_rock" : weapon("Sharp rock", dmg=2),
+            "pointy_stick" : weapon("Pointy stick", dmg=3),
+            "plusone_mace" : weapon("+1 Mace", dmg=4),
+            "warped_blade" : weapon("Warped blade", dmg=5)}.get(ident)
 
 class item_hydration():
 
