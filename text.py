c8bd6f91709de9e9c54a2ef7a5c5f1e0bee33daf
vebjorrs <vebjorrs@ifi.uio.no>
2015-10-25 01:36:59 +0200

    Added descriptions for creatures, areas, and items.

--- text.py
+++ text.py
@@ -6,7 +6,6 @@ intro = ["\"Get on and I'll show you all the sights of the desert!\" the old ",
         "\"That's what happens when you can't pay!\" you hear him shout ",
         "angrily as he disappears behind a sand dune."]
 
-
 flavor = ["A tumbleweed rolls by. The situation is awkward for everyone involved.",
         "A vulture circles you far above, waiting patiently for your inevitable demise.",
         "The distant, melancholy howl of a coyote can be heard on the breeze.",
@@ -23,3 +22,23 @@ monster = {"rattlesnake" : "A rattlesnake hisses menacingly and shakes its tail 
             "coyote" : "A starving coyote jumps out in front of you. It bares its fangs and snarls!",
             "camel" : "A camel! It is probably more afraid of you than you are of it.",
             "skink" : "You found a skink! Dinner time!"}
+
+biome = {0 : "Nothing but sand dunes as far as the eye can see.",
+        1 : "The bedrock pokes out from under the sand here. It should be easy to find some shade.",
+        2 : "Smaller rocks dot the landscape, making it slighly harder to traverse.",
+        3 : "The ground here is treacherously littered with jagged rocks.",
+        4 : "Plants grow around a spring of water. Watch out for thirsty animals.",
+        5 : "An old, abandoned campsite. Hard to tell who was here, or why they left.",
+        6 : "A huge, nondescript pyramid towers above the dunes."}
+
+item = {"raw_meat" : "You strip some raw meat from the dead animal. It's better than nothing.",
+        "cactus_piece" : "You find a cactus, and somehow manage to rip a piece off without getting stung.",
+        "granola_bar" : "You find an unopened granola bar in the rubble. It's still good.",
+        "antidote" : "You find a bottle of antidote! This should come in handy.",
+        "stick" : "You find an old, dry stick lying on the ground.",
+        "sharp_rock" : "You pick up a rock with a sharp edge. It fits perfectly in your hand.",
+        "pointy_stick" : "You find a pointy stick. Perfect for poking animals in the face.",
+        "plusone_mace" : "A can of mace...? Why would anyone bring that out here?",
+        "warped_blade" : "You find a twisted piece of metal, sharp enough to be considered a blade."}
+
+
